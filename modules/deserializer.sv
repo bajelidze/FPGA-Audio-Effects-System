a989118651e7d87d1b24d468f165540f5ea467f7
flashy7 <nflashy1@gmail.com>
2019-03-30 17:11:41 +0400

    rewritten IOs as separate right/left busses, created echo.sv

--- modules/deserializer.sv
+++ modules/deserializer.sv
@@ -1,28 +1,38 @@
-module deserializer(input ADCDAT, inout BCLK, ADCLRCK, output [31:0] ADCDAT_PAR);
-	logic start = 0;
-	logic [5:0] i = 31;
-	logic [31:0] ADCDAT_PAR_Local;
+module deserializer(
+	input ADCDAT,
+	inout BCLK,
+	inout ADCLRCK,
+//	output [31:0] ADCDAT_PAR,
+	output [15:0] leftSample,
+	output [15:0] rightSample
+	);
+	
+logic start = 0;
+logic [5:0] i = 31;
+logic [31:0] ADCDAT_PAR_Local;
 
-	always_ff @(posedge BCLK)begin
-		if(start == 0 && ADCLRCK == 1)begin
-			start = 1;
+always_ff @(posedge BCLK)begin
+	if(start == 0 && ADCLRCK == 1)begin
+		start = 1;
+	end
+	if(start == 1)begin
+		if(ADCLRCK == 1 && i >= 16)begin
+			ADCDAT_PAR_Local[i - 16] = ADCDAT;
+			i = i - 1;
 		end
-		if(start == 1)begin
-			if(ADCLRCK == 1 && i >= 16)begin
-				ADCDAT_PAR_Local[i] = ADCDAT;
+		else if(ADCLRCK == 0 && i < 16)begin
+			ADCDAT_PAR_Local[i] = ADCDAT;
+			if(i != 0)begin
 				i = i - 1;
 			end
-			else if(ADCLRCK == 0 && i < 16)begin
-				ADCDAT_PAR_Local[i] = ADCDAT;
-				if(i != 0)begin
-					i = i - 1;
-				end
-				else begin
-					start = 0;
-					i = 31;
-					ADCDAT_PAR = ADCDAT_PAR_Local;
-				end
+			else begin
+				start = 0;
+				i = 31;
+				leftSample = ADCDAT_PAR_Local[31:16];
+				rightSample = ADCDAT_PAR_Local[15:0];
 			end
 		end
 	end
+end
+
 endmodule
---
+++ modules/echo.sv
@@ -0,0 +1,5 @@
+//module delay(
+//	input [31:0] sample,
+//	input Enable,
+//	output [15:0] left_
+//);
