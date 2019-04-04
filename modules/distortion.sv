881c65238cb1327b2a7496e979b2f1277ee432f3
flashy7 <nflashy1@gmail.com>
2019-04-04 17:51:21 +0400

    first draft of distortion done, works, but with a LOT of noise

--- modules/distortion.sv
+++ modules/distortion.sv
@@ -3,29 +3,34 @@ module distortion(
 	input signed [15:0] rightSampleIn,
 	output signed [15:0] leftSampleOut,
 	output signed [15:0] rightSampleOut,
-	input ADCLRCK,
 	input threshold
 	);
 	
+	logic signed [15:0] left_Local;
+	logic signed [15:0] right_Local;
+	
 	always @(leftSampleIn)begin
-		if(leftSampleIn > 16'd16384)begin
-			leftSampleOut <= 16'd16384;
+		left_Local = leftSampleIn * 2;
+		right_Local = rightSampleIn * 2;
+		
+		if(left_Local > 16'd24576)begin	//0.75
+			leftSampleOut = 16'd24576;
 		end
-		else if(leftSampleIn < -16'd16384)begin
-			leftSampleOut <= -16'd16384;
+		else if(left_Local < -16'd24576)begin
+			leftSampleOut = -16'd24576;
 		end
 		else begin
-			leftSampleOut <= leftSampleIn;
+			leftSampleOut = left_Local;
 		end
 		
-		if(rightSampleIn > 16'd16384)begin
-			rightSampleOut <= 16'd16384;
+		if(right_Local > 16'd24576)begin
+			rightSampleOut = 16'd24576;
 		end
-		else if(rightSampleIn < -16'd16384)begin
-			rightSampleOut <= -16'd16384;
+		else if(right_Local < -16'd24576)begin
+			rightSampleOut = -16'd24576;
 		end
 		else begin
-			rightSampleOut <= rightSampleIn;
+			rightSampleOut = right_Local;
 		end
 	end
 endmodule
