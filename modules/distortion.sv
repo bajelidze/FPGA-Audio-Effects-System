d0d04b93185eba2d6d055130aae88ea901f40b74
flashy7 <nflashy1@gmail.com>
2019-04-04 20:11:41 +0400

    changed distortion.sv

--- modules/distortion.sv
+++ modules/distortion.sv
@@ -6,31 +6,41 @@ module distortion(
 	input threshold
 	);
 	
-	logic signed [15:0] left_Local;
-	logic signed [15:0] right_Local;
+	logic signed [31:0] left_Local = 0;
+	logic signed [31:0] right_Local = 0;
 	
 	always @(leftSampleIn)begin
-		left_Local = leftSampleIn * 2;
-		right_Local = rightSampleIn * 2;
+		left_Local = leftSampleIn;
+		right_Local = rightSampleIn;
+//		left_Local = left_Local >> 1;
+		right_Local = right_Local * 2;
 		
-		if(left_Local > 16'd24576)begin	//0.75
-			leftSampleOut = 16'd24576;
+		if(left_Local > 32767)begin	//0.75
+			leftSampleOut = 16'd32767;
 		end
-		else if(left_Local < -16'd24576)begin
-			leftSampleOut = -16'd24576;
+		else if(left_Local < -32768)begin
+			leftSampleOut = -16'd32768;
 		end
 		else begin
-			leftSampleOut = left_Local;
+			leftSampleOut = left_Local[15:0];
 		end
 		
-		if(right_Local > 16'd24576)begin
-			rightSampleOut = 16'd24576;
+		if(right_Local > 29490)begin
+			rightSampleOut = 16'd29490;
 		end
-		else if(right_Local < -16'd24576)begin
-			rightSampleOut = -16'd24576;
+		else if(right_Local < -29490)begin
+			rightSampleOut = -16'd29490;
 		end
 		else begin
-			rightSampleOut = right_Local;
+			if(right_Local > 32767)begin
+				rightSampleOut = 16'd32767;
+			end
+			else if(right_Local < -32768)begin
+				rightSampleOut = -16'd32768;
+			end
+			else begin
+				rightSampleOut = right_Local[15:0];
+			end
 		end
 	end
 endmodule
