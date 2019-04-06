9829a401c6d6915d6ad7e72f28b45aed3fd9a9f4
flashy7 <nflashy1@gmail.com>
2019-04-06 15:05:15 +0400

    created .sdc timing constraints file

--- modules/distortion.sv
+++ modules/distortion.sv
@@ -10,37 +10,29 @@ module distortion(
 	logic signed [31:0] right_Local = 0;
 	
 	always @(leftSampleIn)begin
-		left_Local = leftSampleIn;
-		right_Local = rightSampleIn;
-//		left_Local = left_Local >> 1;
-		right_Local = right_Local * 2;
+		left_Local <= 0;
+		right_Local <= 0;
+		left_Local <= (leftSampleIn * 12) / 10;
+		right_Local <= rightSampleIn * 2;
 		
-		if(left_Local > 32767)begin	//0.75
-			leftSampleOut = 16'd32767;
+		if(left_Local > 32'd24576)begin	//0.75
+			leftSampleOut <= 16'd24576;
 		end
-		else if(left_Local < -32768)begin
-			leftSampleOut = -16'd32768;
+		else if(left_Local < -32'd24576)begin
+			leftSampleOut <= -16'd24576;
 		end
 		else begin
-			leftSampleOut = left_Local[15:0];
+			leftSampleOut <= left_Local[15:0];
 		end
 		
-		if(right_Local > 29490)begin
-			rightSampleOut = 16'd29490;
+		if(right_Local > 32'd24576)begin
+			rightSampleOut <= 16'd24576;
 		end
-		else if(right_Local < -29490)begin
-			rightSampleOut = -16'd29490;
+		else if(right_Local < -32'd24576)begin
+			rightSampleOut <= -16'd24576;
 		end
 		else begin
-			if(right_Local > 32767)begin
-				rightSampleOut = 16'd32767;
-			end
-			else if(right_Local < -32768)begin
-				rightSampleOut = -16'd32768;
-			end
-			else begin
-				rightSampleOut = right_Local[15:0];
-			end
+			rightSampleOut <= right_Local[15:0];
 		end
 	end
 endmodule
