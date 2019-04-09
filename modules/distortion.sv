b3dae6ad2601ca950392b14b69d47be8259904ff
flashy7 <nflashy1@gmail.com>
2019-04-09 16:15:14 +0400

    added new controlls

--- modules/distortion.sv
+++ modules/distortion.sv
@@ -4,9 +4,8 @@ module distortion(
 	output signed [15:0] leftSampleOut,
 	output signed [15:0] rightSampleOut,
 	input signed [15:0] gainNum,		//Numerator	0-128
-	input signed [15:0] gainDen,	//Denominator 0-128
-	input [1:0] mode, //0 - OFF, 1 - Distortion ON, 2 - Rectifier Distortion ON
-	input threshold
+	input signed [31:0] threshold,
+	input [1:0] mode //0 - OFF, 1 - Distortion ON, 2 - Rectifier Distortion ON
 	);
 	
 	logic signed [31:0] left_Local = 0;
@@ -18,14 +17,53 @@ module distortion(
 			rightSampleOut <= rightSampleIn;
 		end
 		else if(mode == 1)begin		// Distortion ON
-			left_Local <= (leftSampleIn * gainNum) / gainDen;
-			right_Local <= (rightSampleIn * gainNum) / gainDen;
+//			left_Local <= (leftSampleIn * gainNum) / gainDen;
+//			right_Local <= (rightSampleIn * gainNum) / gainDen;
+//			
+//			if(left_Local > threshold)begin	//0.75
+//				leftSampleOut <= threshold[15:0];
+//			end
+//			else if(left_Local < -threshold)begin
+//				leftSampleOut <= -threshold[15:0];
+//			end
+//			else begin
+//				if(left_Local > 32767)begin
+//					leftSampleOut <= 16'd32767;
+//				end
+//				if(left_Local < -32768)begin
+//					leftSampleOut <= -16'd32768;
+//				end
+//				else begin
+//					leftSampleOut <= left_Local[15:0];
+//				end
+//			end
+//			
+//			if(right_Local > threshold)begin
+//				rightSampleOut <= threshold[15:0];
+//			end
+//			else if(right_Local < -threshold)begin
+//				rightSampleOut <= -threshold[15:0];
+//			end
+//			else begin
+//				if(right_Local > 32767)begin
+//					rightSampleOut <= 16'd32767;
+//				end
+//				if(right_Local < -32768)begin
+//					rightSampleOut <= -16'd32768;
+//				end
+//				else begin
+//					rightSampleOut <= right_Local[15:0];
+//				end
+//			end
+
+			left_Local <= leftSampleIn * gainNum;
+			right_Local <= rightSampleIn * gainNum;
 			
-			if(left_Local > 24000)begin	//0.75
-				leftSampleOut <= 16'd24000;
+			if(left_Local > threshold)begin	//0.75
+				leftSampleOut <= threshold[15:0];
 			end
-			else if(left_Local < -24000)begin
-				leftSampleOut <= -16'd24000;
+			else if(left_Local < -threshold)begin
+				leftSampleOut <= -threshold[15:0];
 			end
 			else begin
 				if(left_Local > 32767)begin
@@ -39,11 +77,11 @@ module distortion(
 				end
 			end
 			
-			if(right_Local > 24000)begin
-				rightSampleOut <= 16'd24000;
+			if(right_Local > threshold)begin
+				rightSampleOut <= threshold[15:0];
 			end
-			else if(right_Local < -24000)begin
-				rightSampleOut <= -16'd24000;
+			else if(right_Local < -threshold)begin
+				rightSampleOut <= -threshold[15:0];
 			end
 			else begin
 				if(right_Local > 32767)begin
@@ -56,6 +94,26 @@ module distortion(
 					rightSampleOut <= right_Local[15:0];
 				end
 			end
+			
+//			if(left_Local > 16383)begin	//0.75
+//				leftSampleOut <= 16'd32767;
+//			end
+//			else if(left_Local < -16383)begin
+//				leftSampleOut <= -16'd32767;
+//			end
+//			else begin
+//				leftSampleOut <= leftSampleIn * 2;
+//			end
+//			
+//			if(right_Local > 16383)begin	//0.75
+//				rightSampleOut <= 16'd32767;
+//			end
+//			else if(right_Local < -16383)begin
+//				rightSampleOut <= -16'd32767;
+//			end
+//			else begin
+//				rightSampleOut <= rightSampleIn * 2;
+//			end
 		end
 		else if(mode == 2)begin			//Rectifier Distortion ON
 			if(leftSampleIn < 0) leftSampleOut <= 0;
--- modules/distortion_controller.sv
+++ modules/distortion_controller.sv
@@ -1,11 +1,11 @@
 module distortion_controller(
 	input key3,
 	input key2,
-	input SW9,
-	input SW8,
+	input [1:0] SW,
+//	input SW8,
 	input CLK,
 	output signed [15:0] gainNum,
-	output signed [15:0] gainDen,
+	output signed [31:0] threshold,
 	output [1:0] mode
 	);
 
@@ -16,35 +16,37 @@ module distortion_controller(
 	always @(posedge CLK)begin
 		if(flag == 0)begin
 			gainNum <= 1;
-			gainDen <= 1;
 			flag <= 1;
 			mode <= 0;
+			threshold <= 16000; //remove this
 		end
 		else begin
-			if(key3State == 0 && key3 == 1)begin
-				key3State <= 1;
-			end
-			else if(key3State == 1 && key3 == 0)begin
-				if(gainNum > 1)begin
-					gainNum <= gainNum - 1;
+			if(mode == 1)begin
+				if(key3State == 0 && key3 == 1)begin
+					key3State <= 1;
 				end
-				key3State <= 0;
+				else if(key3State == 1 && key3 == 0)begin
+					if(gainNum > 1)begin
+						gainNum <= gainNum - 1;
+					end
+					key3State <= 0;
+				end
+				
+				if(key2State == 0 && key2 == 1)begin
+					key2State <= 1;
+				end
+				else if(key2State == 1 && key2 == 0)begin
+					if(gainNum < 50)begin
+						gainNum <= gainNum + 1;
+					end
+					key2State <= 0;
+				end
 			end
 			
-			if(key2State == 0 && key2 == 1)begin
-				key2State <= 1;
-			end
-			else if(key2State == 1 && key2 == 0)begin
-				if(gainNum < 50)begin
-					gainNum <= gainNum + 1;
-				end
-				key2State <= 0;
-			end
-			
-			if(SW9 == 1)begin
+			if(SW == 2)begin	//10
 				mode <= 1;
 			end
-			else if(SW8 == 1)begin	//if SW9 == 0 && SW8 == 1
+			else if(SW == 1)begin	//if SW9 == 0 && SW8 == 1	01
 				mode <= 2;
 			end
 			else begin
