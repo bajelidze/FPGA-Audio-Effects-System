fe97726e8508284bd6d1b1cbec26d20b9197dbec
flashy7 <nflashy1@gmail.com>
2019-04-09 18:33:03 +0400

    trying to improve distortion, removed nonblocking assignments because of race conditions

--- modules/distortion.sv
+++ modules/distortion.sv
@@ -3,7 +3,7 @@ module distortion(
 	input signed [15:0] rightSampleIn,
 	output signed [15:0] leftSampleOut,
 	output signed [15:0] rightSampleOut,
-	input signed [15:0] gainNum,		//Numerator	0-128
+	input signed [15:0] gain,		//Numerator	0-128
 	input signed [31:0] threshold,
 	input [1:0] mode //0 - OFF, 1/3 - Distortion ON, 2 - Rectifier Distortion ON
 	);
@@ -13,14 +13,14 @@ module distortion(
 
 	always @(leftSampleIn)begin
 		if(mode == 0)begin			//OFF
-			leftSampleOut <= leftSampleIn;
-			rightSampleOut <= rightSampleIn;
+			leftSampleOut = leftSampleIn;
+			rightSampleOut = rightSampleIn;
 		end
 		else if(mode == 1 || mode == 3)begin		// Distortion ON
-//			left_Local <= (leftSampleIn * gainNum) / gainDen;
-//			right_Local <= (rightSampleIn * gainNum) / gainDen;
+//			left_Local <= leftSampleIn * gain;
+//			right_Local <= rightSampleIn * gain;
 //			
-//			if(left_Local > threshold)begin	//0.75
+//			if(left_Local > threshold)begin
 //				leftSampleOut <= threshold[15:0];
 //			end
 //			else if(left_Local < -threshold)begin
@@ -56,75 +56,45 @@ module distortion(
 //				end
 //			end
 
-			left_Local <= leftSampleIn * gainNum;
-			right_Local <= rightSampleIn * gainNum;
+			left_Local = leftSampleIn;
+			right_Local = rightSampleIn;
 			
-			if(left_Local > threshold)begin	//0.75
-				leftSampleOut <= threshold[15:0];
+			if(left_Local > threshold)begin
+				left_Local = threshold * gain;
+				leftSampleOut = left_Local[15:0];
 			end
 			else if(left_Local < -threshold)begin
-				leftSampleOut <= -threshold[15:0];
+				left_Local = -threshold * gain;
+				leftSampleOut = left_Local[15:0];
 			end
 			else begin
-				if(left_Local > 32767)begin
-					leftSampleOut <= 16'd32767;
-				end
-				if(left_Local < -32768)begin
-					leftSampleOut <= -16'd32768;
-				end
-				else begin
-					leftSampleOut <= left_Local[15:0];
-				end
+				left_Local = left_Local * gain;
+				leftSampleOut = left_Local[15:0];
 			end
 			
 			if(right_Local > threshold)begin
-				rightSampleOut <= threshold[15:0];
+				right_Local = threshold * gain;
+				rightSampleOut = right_Local[15:0];
 			end
 			else if(right_Local < -threshold)begin
-				rightSampleOut <= -threshold[15:0];
+				right_Local = -threshold * gain;
+				rightSampleOut = right_Local[15:0];
 			end
 			else begin
-				if(right_Local > 32767)begin
-					rightSampleOut <= 16'd32767;
-				end
-				if(right_Local < -32768)begin
-					rightSampleOut <= -16'd32768;
-				end
-				else begin
-					rightSampleOut <= right_Local[15:0];
-				end
+				right_Local = right_Local * gain;
+				rightSampleOut = right_Local[15:0];
 			end
-			
-//			if(left_Local > 16383)begin	//0.75
-//				leftSampleOut <= 16'd32767;
-//			end
-//			else if(left_Local < -16383)begin
-//				leftSampleOut <= -16'd32767;
-//			end
-//			else begin
-//				leftSampleOut <= leftSampleIn * 2;
-//			end
-//			
-//			if(right_Local > 16383)begin	//0.75
-//				rightSampleOut <= 16'd32767;
-//			end
-//			else if(right_Local < -16383)begin
-//				rightSampleOut <= -16'd32767;
-//			end
-//			else begin
-//				rightSampleOut <= rightSampleIn * 2;
-//			end
 		end
 		else if(mode == 2)begin			//Rectifier Distortion ON
 			if(leftSampleIn < 0) leftSampleOut <= 0;
 			else begin
-				leftSampleOut <= leftSampleIn;
+				leftSampleOut = leftSampleIn;
 			end
 			if(rightSampleIn < 0)begin
-				rightSampleOut <= 0;
+				rightSampleOut = 0;
 			end
 			else begin
-				rightSampleOut <= rightSampleIn;
+				rightSampleOut = rightSampleIn;
 			end
 		end
 	end
--- modules/distortion_controller.sv
+++ modules/distortion_controller.sv
@@ -4,7 +4,7 @@ module distortion_controller(
 	input [1:0] SW,
 //	input SW8,
 	input CLK,
-	output signed [15:0] gainNum,
+	output signed [15:0] gain,
 	output signed [31:0] threshold,
 	output [1:0] mode
 	);
@@ -15,7 +15,7 @@ module distortion_controller(
 
 	always @(posedge CLK)begin
 		if(flag == 0)begin
-			gainNum <= 1;
+			gain <= 1;
 			flag <= 1;
 			mode <= 0;
 			threshold <= 16000; //remove this
@@ -26,8 +26,8 @@ module distortion_controller(
 					key3State <= 1;
 				end
 				else if(key3State == 1 && key3 == 0)begin
-					if(gainNum > 1)begin
-						gainNum <= gainNum - 1;
+					if(gain > 1)begin
+						gain <= gain - 1;
 					end
 					key3State <= 0;
 				end
@@ -36,8 +36,8 @@ module distortion_controller(
 					key2State <= 1;
 				end
 				else if(key2State == 1 && key2 == 0)begin
-					if(gainNum < 50)begin
-						gainNum <= gainNum + 1;
+					if(gain < 50)begin
+						gain <= gain + 1;
 					end
 					key2State <= 0;
 				end
