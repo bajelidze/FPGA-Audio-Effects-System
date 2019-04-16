d573b830d120f70ab058685f023ab57543ab59d9
flashy7 <nflashy1@gmail.com>
2019-04-16 15:23:16 +0400

    first draft of echo done, noise on right channel

--- modules/echo.sv
+++ modules/echo.sv
@@ -1,54 +1,34 @@
 module echo(
 	input signed [15:0] leftSampleIn,
 	input signed [15:0] rightSampleIn,
-	output signed [15:0] leftSampleOut,
-	output signed [15:0] rightSampleOut,
+	output signed [15:0] sampleOut,
 	
 	input signed [15:0] Q,
 	output signed [15:0] D,
 	output [15:0] write_address,
 	output [15:0] read_address,
-	output W_E
+	output W_E,
+	input ADCLRCK
 	);
 	
-	logic signed [31:0] left_Local = 0;
-	logic signed [31:0] right_Local = 0;
-	
 	logic flag = 0;
-	logic [31:0] counter = 0;
 
-	always @(leftSampleIn)begin
+	always @(posedge ADCLRCK)begin
 		if(flag == 0)begin
 			D <= leftSampleIn;
-			leftSampleOut <= leftSampleIn;
-			rightSampleOut <= leftSampleIn;
+			sampleOut <= leftSampleIn;
 			W_E <= 1;
 			write_address <= 0;
 			read_address <= 0;
+			flag <= 1;
 		end
 		else begin
-			if(counter <= 65535)begin
-				counter <= counter + 1;
-				
-				if(write_address < 65535)
-					write_address <= write_address + 1;
-				else
-					write_address <= 0;
-					
-				D <= leftSampleIn;
-				
-				leftSampleOut <= leftSampleIn;
-				rightSampleOut <= leftSampleIn;
-			end
-			else begin
-				write_address <= write_address + 1;
-				read_address <= read_address + 1;
-				
-				leftSampleOut <= leftSampleIn + (Q / 2);
-				rigthSampleOut <= leftSampleIn + (Q / 2);
-				
-				D <= leftSampleIn;
-			end
+			write_address <= write_address + 1;
+			read_address <= read_address + 1;
+			
+			sampleOut <= leftSampleIn + (Q >>> 1);
+			
+			D <= sampleOut;
 		end
 	end
 	
--- modules/ram.sv
+++ modules/ram.sv
@@ -1,13 +1,13 @@
 module ram(
-	output [15:0] Q,
-	input [15:0] D,
+	output signed [15:0] Q,
+	input signed [15:0] D,
 	input [15:0] write_address,
 	input [15:0] read_address,
 	input W_E,
 	input CLK
 	);
 	
-	logic [15:0] mem [65535:0];
+	logic signed [15:0] mem [65535:0];
 	
 	always @(posedge CLK)begin
 		if(W_E)begin
